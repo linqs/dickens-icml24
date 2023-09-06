39c1be9eb2514570ebbadb7e54619bc6f6021f53
Charles Dickens <charlieadickens@gmail.com>
2023-09-06 12:20:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,23 +9,23 @@ PSL_DATASETS='citeseer'
 function main() {
     trap exit SIGINT
 
-#    ./hlmrf_learning/scripts/setup_psl_examples.sh
-#    for dataset in $PSL_DATASETS; do
-#        echo "Running psl Dual BCD inference regularization experiments on dataset: ${dataset}."
-#        python3 ./hlmrf_learning/scripts/run_dual_bcd_inference_regularization_experiments.py ${dataset}
-#    done
-#
-#    ./hlmrf_learning/scripts/setup_psl_examples.sh
-#    for dataset in $PSL_DATASETS; do
-#        echo "Running psl inference timing experiments on dataset: ${dataset}."
-#        python3 ./hlmrf_learning/scripts/run_inference_timing_experiments.py ${dataset}
-#    done
+    ./hlmrf_learning/scripts/setup_psl_examples.sh
+    for dataset in $PSL_DATASETS; do
+        echo "Running psl Dual BCD inference regularization experiments on dataset: ${dataset}."
+        python3 ./hlmrf_learning/scripts/run_dual_bcd_inference_regularization_experiments.py ${dataset}
+    done
 
-#    ./hlmrf_learning/scripts/setup_psl_examples.sh
-#    for dataset in $PSL_DATASETS; do
-#        echo "Running psl learning timing experiments on dataset: ${dataset}."
-#        python3 ./hlmrf_learning/scripts/run_weight_learning_inference_timing_experiments.py ${dataset}
-#    done
+    ./hlmrf_learning/scripts/setup_psl_examples.sh
+    for dataset in $PSL_DATASETS; do
+        echo "Running psl inference timing experiments on dataset: ${dataset}."
+        python3 ./hlmrf_learning/scripts/run_inference_timing_experiments.py ${dataset}
+    done
+
+    ./hlmrf_learning/scripts/setup_psl_examples.sh
+    for dataset in $PSL_DATASETS; do
+        echo "Running psl learning timing experiments on dataset: ${dataset}."
+        python3 ./hlmrf_learning/scripts/run_weight_learning_inference_timing_experiments.py ${dataset}
+    done
 
     ./hlmrf_learning/scripts/setup_psl_examples.sh
     for dataset in $PSL_DATASETS; do
@@ -33,15 +33,15 @@ function main() {
         python3 ./hlmrf_learning/scripts/run_weight_learning_performance_experiments.py ${dataset}
     done
 
-#    pushd . >/dev/null
-#      cd mnist_addition
-#      python3 ./scripts/run_mnistadd1.py
-#    popd >/dev/null
+    pushd . >/dev/null
+      cd mnist_addition
+      python3 ./scripts/run_mnistadd1.py
+    popd >/dev/null
 
-#    pushd . >/dev/null
-#      cd mnist_addition
-#      python3 ./scripts/run_mnistadd2.py
-#    popd >/dev/null
+    pushd . >/dev/null
+      cd mnist_addition
+      python3 ./scripts/run_mnistadd2.py
+    popd >/dev/null
 }
 
 main "$@"
