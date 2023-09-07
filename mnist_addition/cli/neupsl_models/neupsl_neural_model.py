2c8601914dfb46a664721d14927b2839f4825979
Charles Dickens <charlieadickens@gmail.com>
2023-09-07 12:46:53 -0700

    No DINO loss.

--- mnist_addition/cli/neupsl_models/neupsl_neural_model.py
+++ mnist_addition/cli/neupsl_models/neupsl_neural_model.py
@@ -24,19 +24,8 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
         super().__init__()
         self._application = None
 
-        self._student_model = None
-        self._student_predictions_1 = None
-        self._student_predictions_2 = None
-
-        self._teacher_model = None
-        self._teacher_predictions_1 = None
-        self._teacher_predictions_2 = None
-        self._teacher_parameter_momentum_minimum = 0.996
-        self._teacher_parameter_momentum_maximum = 1.0
-        self._teacher_parameter_momentum = self._teacher_parameter_momentum_maximum
-        self._teacher_parameter_momentum_half_period = 5000
-        self._teacher_center_momentum = 0.9
-        self._teacher_center = None
+        self._model = None
+        self._predictions = None
 
         self._training_transforms = None
         self._optimizer = None
@@ -52,21 +41,16 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
     def internal_init_model(self, application, options={}):
         self._application = application
 
-        student_options = options.copy()
-        student_options['temperature'] = 1.0
-        self._student_model = self._create_model(options=student_options).to(self._device)
-
-        teacher_options = options.copy()
-        teacher_options['temperature'] = 0.04
-        self._teacher_model = self._create_model(options=teacher_options).to(self._device)
-        self._teacher_center = torch.zeros(int(options['class-size']), device=self._device) + 1.0 / int(options['class-size'])
+        options = options.copy()
+        options['temperature'] = 1.0
+        self._model = self._create_model(options=options).to(self._device)
 
         if self._application == 'learning':
             self._iteration = 0
             if options['freeze_resnet'] == 'true':
-                self._optimizer = torch.optim.Adam(self._student_model.mlp.parameters(), lr=float(options['neural_learning_rate']), weight_decay=float(options['weight_decay']))
+                self._optimizer = torch.optim.Adam(self._model.mlp.parameters(), lr=float(options['neural_learning_rate']), weight_decay=float(options['weight_decay']))
             else:
-                self._optimizer = torch.optim.Adam(self._student_model.parameters(), lr=float(options['neural_learning_rate']), weight_decay=float(options['weight_decay']))
+                self._optimizer = torch.optim.Adam(self._model.parameters(), lr=float(options['neural_learning_rate']), weight_decay=float(options['weight_decay']))
 
             self._scheduler = torch.optim.lr_scheduler.StepLR(self._optimizer, step_size=int(options['learning_rate_decay_step']), gamma=float(options['learning_rate_decay']))
 
@@ -80,50 +64,35 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
                     torchvision.transforms.RandomRotation(degrees=0)
                 ])
         elif self._application == 'inference':
-            self._student_model.load_state_dict(torch.load(options['save-path']))
+            self._model.load_state_dict(torch.load(options['save-path']))
 
         return {}
 
     def internal_fit(self, data, gradients, options={}):
         self._prepare_data(data, options=options)
 
-        structured_gradients = float(options["loss_alpha"]) * torch.tensor(gradients.astype(np.float32), dtype=torch.float32, device=self._device)
-
-        dino_loss = (1 - float(options["loss_alpha"])) * (self._dino_loss(self._teacher_predictions_1, self._student_predictions_1)
-                                                          + self._dino_loss(self._teacher_predictions_2, self._student_predictions_2)) / 2.0
+        structured_gradients = torch.tensor(gradients.astype(np.float32), dtype=torch.float32, device=self._device)
 
         self._optimizer.zero_grad()
 
-        dino_loss.backward(retain_graph=True)
-        self._student_predictions_1.backward(structured_gradients, retain_graph=True)
+        self._predictions.backward(structured_gradients, retain_graph=True)
 
         total_gradient_norm = 0.0
-        for p in self._student_model.mlp.parameters():
+        for p in self._model.mlp.parameters():
             param_norm = p.grad.data.norm(2)
             total_gradient_norm += param_norm.item() ** 2
         total_gradient_norm = total_gradient_norm ** (1. / 2)
 
-        torch.nn.utils.clip_grad_norm_(self._student_model.parameters(), 3.0)
+        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 3.0)
 
         self._optimizer.step()
 
-        # EMA updates for the teacher
-        self._update_teacher_momentum()
-        with torch.no_grad():
-            for param_student, param_teacher in zip(self._student_model.parameters(), self._teacher_model.parameters()):
-                param_teacher.data.mul_(self._teacher_parameter_momentum).add_((1 - self._teacher_parameter_momentum) * param_student.detach().data)
-
-            self._teacher_center = (self._teacher_center_momentum * self._teacher_center
-                                    + (1.0 - self._teacher_center_momentum) * torch.cat([self._teacher_predictions_1, self._teacher_predictions_2], dim=0).mean(dim=0))
-
         # Compute the new training loss.
-        new_output = self._student_model(self._features)
+        new_output = self._model(self._features)
 
         loss = torch.nn.functional.cross_entropy(new_output, self._digit_labels).item()
 
         results = {"training_classification_loss": loss,
-                   "dino_loss": dino_loss.item(),
-                   "teacher_center": self._teacher_center.cpu().detach().numpy().tolist(),
                    "gradient_norm_2": total_gradient_norm,
                    "struct gradient_norm_2": torch.norm(structured_gradients, 2).item(),
                    "struct gradient_norm_infty": torch.norm(structured_gradients, torch.inf).item()}
@@ -132,15 +101,6 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
 
         return results
 
-    def _update_teacher_momentum(self):
-        # Update teacher momentum using cosine annealing.
-        self._teacher_parameter_momentum = (self._teacher_parameter_momentum_minimum
-                                            + 0.5 * (self._teacher_parameter_momentum_maximum - self._teacher_parameter_momentum_minimum)
-                                            * (1 + np.cos(np.pi * self._iteration / self._teacher_parameter_momentum_half_period)))
-
-    def _dino_loss(self, teacher_predictions, student_predictions):
-        return -1.0 * (teacher_predictions * torch.log(student_predictions + 1e-7)).sum(dim=1).mean()
-
     def internal_predict(self, data, options={}):
         self._prepare_data(data, options=options)
 
@@ -148,20 +108,14 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
 
         if options["learn"]:
             results['mode'] = 'learning'
-            self._student_model.train()
-            self._student_predictions_1 = self._student_model(self._training_transforms(self._features))
-            self._student_predictions_2 = self._student_model(self._training_transforms(self._features))
-
-            self._teacher_model.train()
-            self._teacher_model.center = self._teacher_center
-            self._teacher_predictions_1 = self._teacher_model(self._training_transforms(self._features)).detach()
-            self._teacher_predictions_2 = self._teacher_model(self._training_transforms(self._features)).detach()
+            self._model.train()
+            self._predictions = self._model(self._training_transforms(self._features))
         else:
             results['mode'] = 'inference'
-            self._student_model.eval()
-            self._student_predictions_1 = self._student_model(self._features)
+            self._model.eval()
+            self._predictions = self._model(self._features)
 
-        return self._student_predictions_1.cpu().detach(), results
+        return self._predictions.cpu().detach(), results
 
     def internal_epoch_end(self, options={}):
         if self._application == 'learning':
@@ -179,7 +133,7 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
 
     def internal_save(self, options={}):
         os.makedirs(os.path.dirname(options['save-path']), exist_ok=True)
-        torch.save(self._student_model.state_dict(), options['save-path'])
+        torch.save(self._model.state_dict(), options['save-path'])
 
         return {}
 
