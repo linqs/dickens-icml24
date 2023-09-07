ebb554ac23689678ede0564740133e9a977bc9ea
Charles Dickens <charlieadickens@gmail.com>
2023-09-07 15:09:32 -0700

    Update simclr_pretrain.py

--- mnist_addition/cli/neupsl_models/simclr_pretrain.py
+++ mnist_addition/cli/neupsl_models/simclr_pretrain.py
@@ -29,16 +29,15 @@ class SimCLR(object):
         self.criterion = torch.nn.CrossEntropyLoss().to(self.device)
 
     def info_nce_loss(self, features):
-        labels = torch.cat([torch.arange(features.shape[0] / 2) for _ in range(2)], dim=0)
+        labels = torch.cat([torch.arange(features.shape[0] / 2, device=self.device) for _ in range(2)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-        labels = labels.to(self.device)
 
         features = torch.nn.functional.normalize(features, dim=1)
 
         similarity_matrix = torch.matmul(features, features.T)
 
         # discard the main diagonal from both: labels and similarities matrix
-        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.device)
+        mask = torch.eye(labels.shape[0], dtype=torch.bool, device=self.device)
         labels = labels[~mask].view(labels.shape[0], -1)
         similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
 
@@ -49,7 +48,7 @@ class SimCLR(object):
         negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
 
         logits = torch.cat([positives, negatives], dim=1)
-        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.device)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long, device=self.device)
 
         logits = logits / self.temperature
         return logits, labels
@@ -60,7 +59,6 @@ class SimCLR(object):
                 tq.set_description("Epoch:{}".format(epoch_counter))
                 for images in tq:
                     images = torch.cat(images, dim=0)
-
                     images = images.to(self.device)
 
                     features = self.model(images)
@@ -81,43 +79,27 @@ class SimCLR(object):
                 self.scheduler.step()
 
 
-class ContrastiveLearningMNIST:
-    def __init__(self, data_folder):
-        self.data_folder = data_folder
+def get_simclr_pipeline_transform():
+    """Return a set of data augmentation transformations fitting the MNIST Dataset."""
+    data_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.RandomRotation(degrees=(0, 90)),
+        torchvision.transforms.ElasticTransform(alpha=125.0),
+        torchvision.transforms.RandomErasing(p=1.0, scale=(0.02, 0.33), ratio=(0.3, 3.3))
+    ])
 
-    @staticmethod
-    def get_simclr_pipeline_transform():
-        """Return a set of data augmentation transformations fitting the MNIST Dataset."""
-        data_transforms = torchvision.transforms.Compose([
-            torchvision.transforms.RandomRotation(degrees=(0, 90)),
-            # torchvision.transforms.RandomPerspective(distortion_scale=0.5, p=1.0),
-            torchvision.transforms.ElasticTransform(alpha=125.0),
-            torchvision.transforms.RandomErasing(p=1.0, scale=(0.02, 0.33), ratio=(0.3, 3.3))
-        ])
+    return data_transforms
 
-        return data_transforms
 
-    def get_dataset(self):
-        """Load the MNIST dataset from the provided data_folder."""
+class ContrastiveLearningMNISTDataset(Dataset):
+    def __init__(self, features):
+        self.features = features
+        self.transform = ContrastiveLearningViewGenerator(get_simclr_pipeline_transform())
 
-        class mnist_dataset(Dataset):
-            def __init__(self, features):
-                self.features = features
-                self.transform = ContrastiveLearningViewGenerator(ContrastiveLearningMNIST.get_simclr_pipeline_transform())
+    def __getitem__(self, index):
+        return self.transform(self.features[index])
 
-            def __getitem__(self, index):
-                return self.transform(self.features[index])
-
-            def __len__(self):
-                return len(self.features)
-
-        training_indices = torch.tensor(pd.read_csv(f"{self.data_folder}/image-target-train.txt", sep="\t", header=None)[0].unique())
-        all_features = pd.read_csv(f"{self.data_folder}/entity-data-map.txt", sep="\t", header=None, index_col=0)
-        training_features = torch.tensor(all_features.loc[training_indices].iloc[:, :-1].values, dtype=torch.float32)
-        training_features = training_features.reshape(-1, 1, 28, 28)
-        training_features = training_features.expand(-1, 3, -1, -1)
-
-        return mnist_dataset(training_features)
+    def __len__(self):
+        return len(self.features)
 
 
 class ContrastiveLearningViewGenerator(object):
@@ -134,8 +116,14 @@ def simclr_pretrain(data_folder):
     """Pre-train a model using SimCLR on MNIST."""
     device = get_torch_device()
 
-    contrastiveLearningMNIST = ContrastiveLearningMNIST(data_folder)
-    mnist_dataset = contrastiveLearningMNIST.get_dataset()
+    training_indices = torch.tensor(
+        pd.read_csv(f"{data_folder}/image-target-train.txt", sep="\t", header=None)[0].unique())
+    all_features = pd.read_csv(f"{data_folder}/entity-data-map.txt", sep="\t", header=None, index_col=0)
+    training_features = torch.tensor(all_features.loc[training_indices].iloc[:, :-1].values, dtype=torch.float32)
+    training_features = training_features.reshape(-1, 1, 28, 28)
+    training_features = training_features.expand(-1, 3, -1, -1)
+
+    mnist_dataset = ContrastiveLearningMNISTDataset(training_features)
 
     resnet18_ = torchvision.models.resnet18()
     resnet18_.conv1 = torch.nn.Conv2d(1, resnet18_.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
@@ -149,7 +137,11 @@ def simclr_pretrain(data_folder):
 
     simclr_pretrain = SimCLR(model.to(device), optimizer, scheduler, temperature=0.07, device=device)
 
-    simclr_pretrain.train(torch.utils.data.DataLoader(mnist_dataset, batch_size=1024, shuffle=True), epochs=1000)
+    simclr_pretrain.train(
+        torch.utils.data.DataLoader(mnist_dataset,
+                                    batch_size=1024, shuffle=True,
+                                    num_workers=2, pin_memory=True),
+        epochs=500)
 
     os.makedirs(f"{data_folder}/saved-networks", exist_ok=True)
 
@@ -170,6 +162,10 @@ def main():
     for experiment in ["mnist-1", "mnist-2"]:
         for split in [0, 1, 2, 3, 4]:
             for train_size in ["00600", "06000", "50000"]:
+                if os.path.exists(f"{THIS_DIR}/../../data/experiment::{experiment}/split::{split}/train-size::{train_size}/overlap::0.00/saved-networks/simclr-pretrained-backbone.pt"):
+                    print(f"Found pretrained models for experiment::{experiment}/split::{split}/train-size::{train_size}/overlap::0.00. Skipping...")
+                    continue
+
                 simclr_pretrain(f"{THIS_DIR}/../../data/experiment::{experiment}/split::{split}/train-size::{train_size}/overlap::0.00")
 
 
