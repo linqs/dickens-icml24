785d92dec3889562e42b2d0ddbf0f2a90e3057b5
Charles Dickens <charlieadickens@gmail.com>
2023-09-12 18:22:58 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
-PSL_DATASETS='epinions citeseer cora drug-drug-interaction yelp'
+PSL_DATASETS='epinions stance-4forums stance-createdebate citeseer cora drug-drug-interaction yelp'
 
 function main() {
     trap exit SIGINT
