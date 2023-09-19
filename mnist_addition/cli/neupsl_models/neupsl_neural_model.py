45d5b6dc669fcc1805557639c3850352873325d3
Charles Dickens <charlieadickens@gmail.com>
2023-09-19 06:03:13 -0700

    Update neupsl_neural_model.py

--- mnist_addition/cli/neupsl_models/neupsl_neural_model.py
+++ mnist_addition/cli/neupsl_models/neupsl_neural_model.py
@@ -162,9 +162,10 @@ class MNISTAdditionModel(pslpython.deeppsl.model.DeepModel):
 
 
 def get_torch_device():
-    if torch.cuda.is_available():
-        return torch.device("cuda")
-    elif torch.backends.mps.is_available():
-        return torch.device("mps")
-    else:
-        return torch.device("cpu")
+    return torch.device("cpu")
+    # if torch.cuda.is_available():
+    #     return torch.device("cuda")
+    # elif torch.backends.mps.is_available():
+    #     return torch.device("mps")
+    # else:
+    #     return torch.device("cpu")
